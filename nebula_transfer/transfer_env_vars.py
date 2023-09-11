2c957358c67de1985010c16076c9ea7a506fcb18
ophie200 <nancydwong@gmail.com>
2023-09-11 04:36:05 -0700

    add json file intermediary to deal with secrets

--- nebula_transfer/transfer_env_vars.py
+++ nebula_transfer/transfer_env_vars.py
@@ -1,4 +1,6 @@
 import os
+import json
+import click
 from sgqlc.endpoint.requests import RequestsEndpoint
 
 
@@ -36,13 +38,15 @@ def get_deployment_id_query(workspace_id, release_name):
     return query
 
 
-def set_env_var_query(deployment_id, release_name, env_vars):
+def get_env_var_str(env_vars):
     env_var_list = []
     [env_var_list.append('{key: "' + env_var['key'] + '", value: "' + env_var['value'] + '", isSecret: ' + str(env_var['isSecret']).lower() + '}') for env_var in env_vars]
     env_vars_str = ', '.join(env_var_list)
     
-    print(env_vars_str)
-        
+    return env_vars_str
+    
+    
+def set_env_var_query(endpoint, deployment_id, release_name, env_vars, upsert_batch_size):
     query = '''
             mutation UpdateDeploymentVariables {
                 updateDeploymentVariables(
@@ -57,13 +61,18 @@ def set_env_var_query(deployment_id, release_name, env_vars):
                     isSecret
                 }
             }
-        '''.replace("$deploymentId", deployment_id).replace("$releaseName", release_name).replace("$envVars", env_vars_str)
-    # print(query)
-        
-    return query
+        '''.replace("$deploymentId", deployment_id).replace("$releaseName", release_name)
+    
+    env_vars_ct = len(env_vars)
+    for i in range(0, env_vars_ct, upsert_batch_size):        
+        env_var_str = get_env_var_str(env_vars[i:min(env_vars_ct, i+upsert_batch_size)])
+        env_vars_response = endpoint(query.replace("$envVars", env_var_str))
 
 
-def main():
+@click.command()
+@click.option('--env_var_json_file', default=None, help='optional json formatted environment variables file')
+#@click.option('--upsert_batch_size', default=10 help='specify upsert batch size, default 10')
+def main(env_var_json_file): 
     try: 
         token = os.environ["NEBULA_DEPLOYMENT_TOKEN"]
         
@@ -78,18 +87,25 @@ def main():
         headers = {"authorization": token}
         endpoint = RequestsEndpoint(url, headers)
         
-        # run query
-        get_env_vars_response = endpoint(get_env_var_query(source_workspace_id, source_release_name))
-        env_vars = get_env_vars_response['data']['workspaceDeployments'][0]['environmentVariables']
-        # [print(env_var) for env_var in env_vars]
+        if env_var_json_file:
+            print("loading env vars from json file")
+            env_vars = json.load(open(env_var_json_file))
+            
+        else:
+            print("getting env vars from graphql")
+            get_env_vars_response = endpoint(get_env_var_query(source_workspace_id, source_release_name))
+            env_vars = get_env_vars_response['data']['workspaceDeployments'][0]['environmentVariables']
+        
+        [print(env_var) for env_var in env_vars]
 
         get_deployment_id_response = endpoint(get_deployment_id_query(dest_workspace_id, dest_release_name))
         deployment_id = get_deployment_id_response['data']['workspaceDeployments'][0]['id']
+        print(deployment_id)
 
-        # will overwrite existing matching keys with
-        # although call to endpoint returns, actual deployment of update will take several seconds - check UI
-        env_vars_response = endpoint(set_env_var_query(deployment_id, dest_release_name, env_vars))
-        print(env_vars_response)
+        # running mutation overwrites all existing env vars
+        # although call to endpoint returns immediately, actual deployment of update will take several seconds - check UI
+        upsert_batch_size=len(env_vars)
+        set_env_var_query(endpoint, deployment_id, dest_release_name, env_vars, upsert_batch_size)
 
     except Exception as e:
         print(f"exception occurred: {e}")
